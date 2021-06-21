aa0466358b7be94efb06523d90fd00f20bbcc25c
narunkorn <Narunkorn_Vi@hotmail.com>
2021-06-21 21:42:21 +0700

    updateHome3-3

--- day_3_home/home_3-3.js
+++ day_3_home/home_3-3.js
@@ -1,13 +1,44 @@
-function cloneArray(arr) {
-    let result = arr * 2
-    return result;
+function clone(arr) {
+    let newArr = new Object()
+    for (let key in arr) {
+        if (typeof (arr[key]) == 'object' && typeof (arr[key]) == 'array') {
+            newArr[key] = clone(arr[key])
+        } else {
+            newArr[key] = arr[key]
+        }
+    }
+    return newArr
 }
-let newarr = cloneArray(4)
-console.log(newarr)
+let input1 = [1, 2, 3]
+let input2 = { a: 1, b: 2 }
+let input3 = [1, 2, { a: 1, b: 2 }]
+let input4 = [1, 2, { a: 1, b: { c: 3, d: 4 } }]
+
+let a = clone(input4)
+a[2] = 8
+// newinput[0] = 8
+console.log(input4, 'input1')
+console.log(a, "newinput")
 
 
 
 
+
+
+
+
+
+
+
+
+
+// $(document).ready(function () {
+
+//     let test = 'dkdkdkd'
+
+
+//     $('#myresult').append($(test));
+// })
 
 
 
